f04df55503782db7bc74ac1eb8c63b229895ba51
Fabian Orland <orland@itc.rwth-aachen.de>
2025-07-30 10:35:41 +0200

    updated installation for Rocky9 on CLAIX-2023

--- install.sh
+++ install.sh
@@ -3,10 +3,30 @@
 # exit the script on error
 set -e
 
-if [ -n "${FORTRAN_ML_VENV}" ]; then
-    echo "Found Python virtual environment: ${FORTRAN_ML_VENV}. Will use for installation."    
+# if [ -n "${FORTRAN_ML_VENV}" ]; then
+#     echo "Found Python virtual environment: ${FORTRAN_ML_VENV}. Will use for installation."    
+# else
+#     echo "Error: No Python virtual environment defined. Please set FORTRAN_ML_ENV to the path of your virtual environment!"
+# fi
+
+if [ ! -f "${FORTRAN_ML_VENV}/bin/activate" ]; then
+    echo "No Python virtual environment found. Installing now!"
+
+    python3 -m venv "${FORTRAN_ML_VENV}"
+    source "${FORTRAN_ML_VENV}/bin/activate"
+
+    pip install --no-cache-dir --no-binary :all: mpi4py==3.1.6
+    pip install Cython
+    pip install tensorflow==2.17.0
+    pip install tf-keras==2.17.0
+
+    pip install matplotlib
+    pip install scipy
+
+    pip install scorep
 else
-    echo "Error: No Python virtual environment defined. Please set FORTRAN_ML_ENV to the path of your virtual environment!"
+    echo "Found Python virtual environment! Nothing to install. Activating environment now!"
+    source "${FORTRAN_ML_VENV}/bin/activate"
 fi
 
 if command -v readlink >/dev/null 2>&1; then
@@ -74,7 +94,7 @@ if [ ! -f "${FORTRAN_ML_ROOT}/extern/aixeleratorservice/BUILD/lib/libAIxelerator
 
     cd ${FORTRAN_ML_ROOT}/extern/aixeleratorservice/
     mkdir -p BUILD && cd BUILD
-    cmake .. -DWITH_TORCH=OFF -DWITH_TENSORFLOW=ON -DTensorflow_DIR=${FORTRAN_ML_ROOT}/extern/tensorflow/ -DTensorflow_Python_DIR=${FORTRAN_ML_VENV}/lib/python3.11/site-packages/tensorflow
+    cmake .. -DWITH_TORCH=OFF -DWITH_TENSORFLOW=ON -DTensorflow_DIR=${FORTRAN_ML_ROOT}/extern/tensorflow/ -DTensorflow_Python_DIR=${FORTRAN_ML_VENV}/lib/python${FORTRAN_ML_PYVERSION}/site-packages/tensorflow
     cmake --build . -j && cmake --install .
 
     echo "AIxeleratorService installation finished!"
@@ -88,7 +108,7 @@ if [ ! -f "${FORTRAN_ML_ROOT}/BUILD/lib/libmlCoupling.so" ]; then
 
     cd ${FORTRAN_ML_ROOT}
     mkdir -p BUILD && cd BUILD
-    cmake .. -Dh5fortran_ROOT=${FORTRAN_ML_ROOT}/extern/h5fortran/BUILD/INSTALL -DTensorflow_DIR=${FORTRAN_ML_ROOT}/extern/tensorflow/ -DTensorflow_Python_DIR=${FORTRAN_ML_VENV}/lib/python3.11/site-packages/tensorflow -DWITH_AIX=ON -DWITH_PHYDLL=ON -DWITH_NCSA=ON
+    cmake .. -Dh5fortran_ROOT=${FORTRAN_ML_ROOT}/extern/h5fortran/BUILD/INSTALL -DTensorflow_DIR=${FORTRAN_ML_ROOT}/extern/tensorflow/ -DTensorflow_Python_DIR=${FORTRAN_ML_VENV}/lib/python${FORTRAN_ML_PYVERSION}/site-packages/tensorflow -DWITH_AIX=ON -DWITH_PHYDLL=ON -DWITH_NCSA=ON
     cmake --build . -j && cmake --install .
 
     echo "Fortran-ML-Interface installation finished!"
--- setupEnvRWTHClaix23.sh
+++ setupEnvRWTHClaix23.sh
@@ -1,28 +1,26 @@
 #!/bin/zsh
 
 module purge
-#module load intel/2023b
+
 module load intel-compilers/2023.1.0
 module load impi/2021.9.0
-#export MPICH_F90=ifx
-#export MPICH_CC=icx
-#export MPICH_CXX=icpx
-export FC=mpiifort
-export CC=mpiicc
-export CXX=mpiicpc
-module load imkl/2023.1.0
-module load Score-P/8.4
+module load intel/2024a
+export FC=mpiifx
+export CC=mpiicx
+export CXX=mpiicpx
 
-module load HDF5/1.14.0
-#module load GCC/12.3.0
-#module load OpenBLAS/0.3.23
+module load HDF5/1.14.5
 
-module load Python/3.11.3
+module load Python/3.12.3
+export FORTRAN_ML_PYVERSION="3.12" # keep consistent with loaded module
+
 module load CUDA/12.3.0
 module load cuDNN/8.9.7.29-CUDA-12.3.0
 module load CMake
 
-export FORTRAN_ML_VENV=${PWD}/venv-fortran-ml-py3113
+module load Score-P/9.2
+
+export FORTRAN_ML_VENV=${PWD}/venv-fortran-ml-py3123
 source ${FORTRAN_ML_VENV}/bin/activate
 
 if command -v readlink >/dev/null 2>&1; then
